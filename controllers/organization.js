7eb0329dbec98fbfbced4dfcb87ec790d49eceb3
Giovanni Mendoza <mendozagioo@gmail.com>
2018-12-20 18:59:28 -0600

    Llenando datos dummy a la vista de organización

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,5 +1,9 @@
+const organizations = require('../public/datas/organizations')
+
 module.exports = {
   init(req, res) {
-    res.render('organization')
+    res.render('organization', {
+      organizationItems: organizations
+    })
   }
 }
