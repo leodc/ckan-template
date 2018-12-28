e186362f44a1e4966997844f08b535bcb5d0b114
Giovanni Mendoza <mendozagioo@gmail.com>
2018-12-27 19:12:17 -0600

    Se agrega atributo de parámetro al json de la vista

--- controllers/dataset.js
+++ controllers/dataset.js
@@ -3,6 +3,7 @@ const datasetItems = require('../public/datas/dataset-items')
 module.exports = {
   init(req, res) {
     res.render('dataset/dataset', {
+      params: req.query,
       datasets: datasetItems,
       breadcrumbs: [{
         name: "Datos",
