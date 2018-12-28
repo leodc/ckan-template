2f92ecd35c59977b95b523f1c4f4aae433980d31
Giovanni Mendoza <mendozagioo@gmail.com>
2018-12-28 09:54:33 -0600

    Se agregan los filtrados dinámicos para el sidebar

--- controllers/dataset.js
+++ controllers/dataset.js
@@ -1,9 +1,11 @@
 const datasetItems = require('../public/datas/dataset-items')
+const sidebarItems = require('../public/datas/sidebar-items')
 
 module.exports = {
   init(req, res) {
-    res.render('dataset/dataset', {
+    res.render('dataset', {
       params: req.query,
+      filters: sidebarItems,
       datasets: datasetItems,
       breadcrumbs: [{
         name: "Datos",
