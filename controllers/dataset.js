7f5a54fdae9b06c4e4978e9fc02d71e799613dbb
Leonel Castañeda <leodcastaneda@gmail.com>
2019-01-06 23:33:30 -0600

    add package_search call

--- controllers/dataset.js
+++ controllers/dataset.js
@@ -1,3 +1,5 @@
+const https = require("https");
+
 module.exports = {
   init(req, res) {
     res.render('dataset', {
@@ -12,18 +14,86 @@ module.exports = {
   },
 
   resource(req, res) {
-    res.render('resource', {
-      source: require('../public/datas/one-resource'),
-      breadcrumbs: [{
-        name: "Instituciones",
-        link: "/organization"
-      }, {
-        name: "CONADIC",
-        link: "/organization/conadic"
-      }, {
-        name: "Convenios firmados en los estados.",
-        link: ""
-      }]
-    })
+    var resourceName = req.params.resource;
+    var url = process.env.CKAN_PACKAGE_SEARCH + "?q=" + resourceName;
+
+    https.get(url, (resp) => {
+      let data = '';
+
+      resp.on('data', (chunk) => {
+        // A chunk of data has been recieved.
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        data = JSON.parse(data);
+        // The whole response has been received. Print out the result.
+        if(data.result.count > 0){
+          var resource;
+          var organizationName;
+          var organizationTitle;
+          var resourceTitle;
+          for(var result of data.result.results){
+            if(result.name == resourceName){
+              resource = result;
+
+              resourceTitle = result.title;
+              organizationTitle = result.organization.title;
+              organizationName = result.organization.name;
+              break;
+            }
+          }
+
+          resource.socials = [
+            {
+              "name": "Google+",
+              "url": "https://plus.google.com/share?url=https://datos.gob.mx/busca/dataset/" + resourceName
+            },
+            {
+              "name": "Twitter",
+              "url": "https://twitter.com/share?url=https://datos.gob.mx/busca/dataset/" + resourceName
+            },
+            {
+              "name": "Facebook",
+              "url": "https://www.facebook.com/sharer.php?u=https://datos.gob.mx/busca/dataset/" + resourceName
+            }
+          ];
+
+          resource.metadatas= {
+            "mail": "judith.ortega@salud.gob.mx",
+            "frequency": "irregular",
+            "guid": "convenios-firmados-en-los-estados",
+            "language": "es",
+            "modified": "2018-03-31T00:00:00Z",
+            "publisher_name": "CONADIC",
+            "publisher_type": "Subdirectora",
+            "published": "2017-10-20T16:19:19Z",
+            "topic": "Salud",
+            "url": "http://www.conadic.salud.gob.mx/prueba/convenios.csv"
+          }
+
+          res.render('resource', {
+            source: resource,
+            breadcrumbs: [{
+              name: "Instituciones",
+              link: "/organization"
+            }, {
+              name: organizationName,
+              link: "/organization/" + organizationTitle
+            }, {
+              name: resourceTitle,
+              link: ""
+            }]
+          });
+
+        }else{
+          // package not found
+        }
+
+      });
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+
   }
 }
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const router = require('./config/router')
 
 let port = process.env.PORT || 4000
 
+process.env.CKAN_PACKAGE_SEARCH="https://datos.gob.mx/ckan-admin/api/3/action/package_search"
+
 app.set('view engine', 'pug')
 app.set('views', 'views')
 app.use(express.static('public'))
